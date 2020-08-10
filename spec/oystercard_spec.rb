4b2c989ba817e7269bf7feff8774582bd8c2660f
imogenkraak <imogen.kraak@gmail.com>
2020-08-10 14:56:12 +0100

    Balance method added

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,12 @@
 require 'oystercard'
 describe Oystercard do
+  it 'oystercard to interact with balance' do
+    card = Oystercard.new
+    expect(card).to respond_to(:balance)
+  end
+
+  it 'has a balance of zero' do
+    expect(subject.balance).to eq(0)
+  end
 
 end
